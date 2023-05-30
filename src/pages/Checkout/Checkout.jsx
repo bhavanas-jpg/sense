f701c3f14abb90474fe7dbd78f91af4508de8176
pavan6cs <bhavanas904@gmail.com>
2023-05-31 00:19:31 +0530

    style: checkout page

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,34 +1,59 @@
 import React from "react";
 import CheckoutAddress from "./CheckoutAddress";
 import { useAddress } from "../../context/AddressContext";
+import "./Checkout.css"
+import { useData } from "../../context/DataContext";
 
 const Checkout = () => {
   const {
     addressListState: { selectedAddress },
   } = useAddress();
+  const {state :{cartProducts}} = useData();
+  const productPrice=(price, qty) => Number(price) * qty;
+  const totalPrice = cartProducts.reduce(
+    (acc, curr) => (acc += Number(curr.price) * curr.qty),
+    0
+  );
+  const discount = Math.floor(totalPrice * 20 /100 , 2);
+  const subTotal = totalPrice - discount;
 
   return (
     <div className="container hg-100">
-      <div style={{ display: "flex", gap: "16rem" }}>
-        <div>
+      <div style={{ display: "flex",gap: "4rem" }}>
+        <div className="checkout-address-card">
           <CheckoutAddress />
         </div>
-        <div>
-          <p>order details : </p>
+        <div className="checkout-card">
+          <h3 className="txt-algn--center mb-1">Order Details  </h3>
           <div style={{ borderBottom: "1px solid #000" }}>
-            <p>purchased items</p>
-            <p>item 1: 1 X 100= 100</p>
+            <h4>Purchased Items</h4>
+            {
+              cartProducts.map((product)=>(
+            <div key={product._id}>
+            <p style={{display:"flex", justifyContent:"space-between"}}>
+              <span className="purchased-product"> {product.name}</span>
+           * 
+           <span> {product.qty}</span>
+           =
+           <span>{productPrice(product.price,product.qty)}</span>
+        
+          </p>
           </div>
-          <div style={{ borderBottom: "1px solid #000" }}>
-            <p>Billing</p>
-            <p>Total MRP</p>
-            <p>Total discount</p>
-            <p>delivery fee: free</p>
+        ))
+      } 
           </div>
-          <div style={{ borderBottom: "1px solid #000" }}>
-            <p>delivering to:</p>
-            <p>selected address come here</p>
-
+          <h4>Billing</h4>
+          <div 
+          className="order-billing"
+          style={{ borderBottom: "1px solid #000" }}>
+            
+            <p>Total MRP <span>${totalPrice}</span>  </p>
+            <p>Total discount  <span> - ${discount}</span> </p>
+            <p>delivery fee: <span>Free</span> </p>
+            <p>Sub Total <span>${subTotal}</span>  </p>
+          </div>
+          <div >
+            <h4>Delivering To:</h4>
             <div>
               <p>
                 <b>{selectedAddress.name}</b>
@@ -42,6 +67,7 @@ const Checkout = () => {
               </p>
               <p>Phone Number: {selectedAddress.phone}</p>
             </div>
+            <button>Place Order</button>
           </div>
         </div>
       </div>
